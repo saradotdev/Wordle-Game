aa16dd94b088dff8aac5b3d859b9b929a7caed83
saraazizhere <saraaziz180903@gmail.com>
2024-03-23 03:18:58 +0500

    handled backspace and enter

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// for light and dark themes
 toggleButton = document.querySelector(".toggle-btn");
 inputs = document.querySelectorAll("input");
-let i = 0;
+inputRows = document.querySelectorAll(".input-row");
+let secretWord;
 
-function toggleMode(event) {
+function toggleMode(event) { // for light and dark themes
     let buttonImgPath = event.target.src;
     if (buttonImgPath.includes("dark-mode")) {
         event.target.src = "light-mode.png";
@@ -20,21 +20,67 @@ function isLetter(value) {
 }
 
 function handleKeyStroke(value, id) {
-    if (isLetter(value) || value === "Backspace" || value === "Enter") {
+    if (isLetter(value)) {
         inputs[parseInt(id) + 1].focus();
-    } else {
     }
 }
 
-function init() {
+function handleBackspace(id) {
+    if (inputs[parseInt(id)].value.length === 0) {
+        inputs[parseInt(id) - 1].focus();
+    }
+}
+
+function handleEnter(id) {
+    letterBoxes = document.querySelectorAll(`#${id} .input-letter`);
+
+    let wordFormed = "";
+    letterBoxes.forEach(letterBox => {
+        if (letterBox.value.length === 1) {
+            wordFormed += letterBox.value;
+        }
+    });
+
+    if (wordFormed.length === 5) {
+        if(validateWord(wordFormed)) {
+            // 
+        }
+    }
+}
+
+async function validateWord(word) {
+    const promise = await fetch("https://words.dev-apis.com/validate-word", {
+        method: "POST",
+        body: JSON.stringify({"word": word})
+    });
+    const response = await promise.json();
+    const isValid = response["validWord"];
+    return isValid;
+}
+
+async function init() {
+    const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const response = await promise.json();
+    secretWord = response["word"];
+
     toggleButton
         .addEventListener("click", function(event) {
             toggleMode(event);
         });
+    
+    inputs[0].focus(); // for focusing on the first input box by default, for user to type in
 
     inputs.forEach(input => {
-        input.addEventListener("keyup", function(event) {
-            handleKeyStroke(event.target.value, event.target.id);
+        input.addEventListener("keydown", function(event) {
+            if (event.key === "Backspace") {
+                handleBackspace(event.target.id);
+            } else if (event.key === "Enter") {
+                handleEnter(event.target.parentElement.id);
+            } else if (isLetter(event.key)) {
+                handleKeyStroke(event.target.value, event.target.id); // input letter
+            } else {
+                event.preventDefault();
+            }
         })
     })
 }
